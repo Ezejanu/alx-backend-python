974f9d3266eb6571443af776f7e9fa6fb5808cc5
Ezejanu <Ezejanu_nno@ymail.com>
2024-02-16 01:15:55 +0000

    task 6 - fixed new PEP8 error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -9,7 +9,8 @@ def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     Calculates the sum of a list of integers and floats.
 
     Parameters:
-    - mxd_lst (List[Union[int, float]]): The input list of integers and float numbers.
+    - mxd_lst (List[Union[int, float]]):
+    The input list of integers and float numbers.
 
     Returns:
     float: The sum of the input list.
